2635ca77d8b2b0115e5486ffb396d1ac957e9b3c
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-17 23:34:07 -0500

    fix: :bug: the new message not found
    the new user creation response message has been changed, as it generated an error when the data was duplicated.
    
    41562e8

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -31,15 +31,7 @@ export class UsersController {
   createUser(@Body() createUserDto: CreateUserDto) {
     try {
       const newUser = this.userServiceClient.send('createUser', createUserDto);
-      if (newUser)
-        return {
-          success: true,
-          message: 'Usuario creado exitosamente.',
-        };
-      return {
-        success: false,
-        message: 'Hubo un error al crear el usuario',
-      };
+      return newUser;
     } catch (error) {
       // console.log(newUser);
       // console.log(error);
