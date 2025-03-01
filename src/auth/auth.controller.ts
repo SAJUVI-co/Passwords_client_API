450fa1617da821154eabd4bea8d0fbe06ac96710
CODE-G-ROOT <juanavila8856@gmail.com>
2025-03-01 05:22:10 -0500

    feat: :sparkles: Enhance UsersController with new user management features and JWT token generation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,101 +6,114 @@ import {
   Delete,
   Body,
   Query,
-  // Param,
   Inject,
   NotFoundException,
   ParseIntPipe,
   DefaultValuePipe,
-  // NotFoundException,
-  // NotAcceptableException,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserRole } from './entities/user.entity';
 import { LoginUserDto } from './dto/login-user.dto';
+import { AuthService, Public } from './auth.service';
+import { DateEnum } from './dto/query-user.dto';
 
 @Controller('users') // Prefijo para las rutas
 export class UsersController {
   constructor(
     @Inject('USERS_SERVICE') private readonly userServiceClient: ClientProxy, // Cliente del microservicio
+    private authService: AuthService, // Cliente del microservicio
   ) {}
 
   // POST /users
+  @Public()
   @Post() //!check
   createUser(@Body() createUserDto: CreateUserDto) {
     try {
       const newUser = this.userServiceClient.send('createUser', createUserDto);
       return newUser;
     } catch (error) {
-      // console.log(newUser);
-      // console.log(error);
       return new NotFoundException(error);
     }
   }
 
   //? SE NECESITAN LOS ROLES PARA DAR ACCEESO A ESTE METODO
-  @Get() //!check
+
+  @Get()
   findAllUsers(
     @Query('skip', new DefaultValuePipe(0), ParseIntPipe) skip: string,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: string,
-    @Query('order', new DefaultValuePipe('ASC'))
-    order: string,
+    @Query('order', new DefaultValuePipe('ASC')) order: string,
+    @Body() loginUserDto: LoginUserDto,
   ) {
     return this.userServiceClient.send(
       { cmd: 'findAllUsers' },
-      { skip, limit, order },
+      { skip, limit, order, loginUserDto },
     );
   }
-
-  @Get('/all') //!check
-  findAll() {
-    return this.userServiceClient.send({ cmd: 'findAll' }, {});
-  }
-
-  // GET /users
-  // @Get('test')
-  // test() {
-  //   const res = this.userServiceClient.send('tests', {});
-  //   if (!res) return new NotAcceptableException('Error del servicio');
-  //   return res;
-  // }
 
   //? SE NECESITAN LOS ROLES PARA DAR ACCEESO A ESTE METODO
   // GET /users/sorted-by-creation
-  @Get('sorted-by-creation')
-  findAllSortedByCreation(@Query('order') order: 'ASC' | 'DESC') {
+  @Get('sorted-by-date')
+  findAllSortedByDate(
+    @Query('skip') skip: number,
+    @Query('limit') limit: number,
+    @Query('order') order: 'ASC' | 'DESC',
+    @Query('date') date: DateEnum,
+  ) {
     return this.userServiceClient.send(
-      { cmd: 'findAllSortedByCreation' },
-      order,
+      { cmd: 'findAllSortedByDate' },
+      { skip, limit, order, date },
     );
-  }
-
-  //? SE NECESITAN LOS ROLES PARA DAR ACCEESO A ESTE METODO
-  // GET /users/sorted-by-update
-  @Get('sorted-by-update')
-  findAllSortedByUpdate(@Query('order') order: 'ASC' | 'DESC') {
-    return this.userServiceClient.send({ cmd: 'findAllSortedByUpdate' }, order);
   }
 
   //? SE NECESITAN LOS ROLES PARA DAR ACCEESO A ESTE METODO
   // GET /users/online //!check
+
   @Get('online')
-  findOnlineUsers() {
-    return this.userServiceClient.send({ cmd: 'findOnlineUsers' }, {});
+  findOnlineUsers(
+    @Query('skip') skip: number,
+    @Query('limit') limit: number,
+    @Query('order') order: 'ASC' | 'DESC',
+  ) {
+    return this.userServiceClient.send(
+      { cmd: 'findOnlineUsers' },
+      { skip, limit, order },
+    );
   }
 
   //? SE NECESITAN LOS ROLES PARA DAR ACCEESO A ESTE METODO
   // GET /users/role
   @Get('role')
-  findUsersByRole(@Query('role') role: UserRole) {
-    return this.userServiceClient.send({ cmd: 'findUsersByRole' }, role);
+  findUsersByRole(
+    @Query('role') role: UserRole,
+    @Query('skip') skip: number,
+    @Query('limit') limit: number,
+    @Query('order') order: 'ASC' | 'DESC',
+  ) {
+    return this.userServiceClient.send(
+      { cmd: 'findUsersByRole' },
+      { role, skip, limit, order },
+    );
   }
 
   // Post /users/login //!check
+  @HttpCode(HttpStatus.OK)
   @Post('login')
   findOneUser(@Body() loginUserDto: LoginUserDto) {
-    return this.userServiceClient.send('login', loginUserDto);
+    const user = this.userServiceClient.send('login', loginUserDto);
+    return user;
+  }
+
+  // Post /users/login //!check
+  @Public()
+  @Post('token')
+  tokenGenerate(@Body() user) {
+    const token = this.authService.jsonwebToken(user);
+    return token;
   }
 
   //? SE NECESITAN LOS ROLES PARA DAR ACCEESO A ESTE METODO
@@ -116,8 +129,14 @@ export class UsersController {
     return this.userServiceClient.send('updateUser', updateUserDto);
   }
 
+  // Soft Delete /users/:id
+  @Delete('delete-User')
+  deleteUser(@Body('id') id: number) {
+    return this.userServiceClient.send('deleteUser', id);
+  }
+
   // DELETE /users/:id
-  @Delete('delete')
+  @Delete('remove-User')
   removeUser(@Body('id') id: number) {
     return this.userServiceClient.send('removeUser', id);
   }
